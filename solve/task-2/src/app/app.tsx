bfd5f8d9088378d62c2c1ace9ecf88d51224d9ed
shinoki <endernasha85@gmail.com>
2023-10-09 19:25:14 +0300

    task 2 p.2

--- solve/task-2/src/app/app.tsx
+++ solve/task-2/src/app/app.tsx
@@ -1,21 +1,68 @@
 import styles from './app.module.scss';
 import i_images from '@assets/images';
 
+import { useEffect, useState } from 'react';
+import { getLocaleDate } from '@/utils/getLocaleDate';
+import { createPortal } from 'react-dom';
+import Modal from '@/components/modal/modal';
+
 const images = Object.entries(i_images);
 
+interface IItem {
+	title: string;
+	src: string;
+	handleImage: (title: string, src: string) => void;
+	handleCross: () => void;
+}
+
+const Item: React.FC<IItem> = ({ title, src, handleImage, handleCross }) => {
+	return (
+		<div className={styles.item} onClick={() => handleImage(title, src)}>
+			<img src={src} alt={title} />
+		</div>
+	);
+};
+
 const App = () => {
-	console.log(images);
+	const [image, setImage] = useState<{ title: string; src: string }>({
+		title: '',
+		src: '',
+	});
+	const [shouldShow, setShouldShow] = useState<boolean>(false);
+
+	const handleModalImage = (title: string, src: string) => {
+		setImage({ title, src });
+		setShouldShow(true);
+	};
 
 	return (
-		<div className={styles.wrapper}>
-			<div className={styles.container}>
-				{images.map(([title, src]) => (
-					<div className={styles.item}>
-						<img key={title} src={src} alt={title} />
-					</div>
-				))}
+		<>
+			<div className={styles.header}>
+				<p>Images {images.length}</p>
+				<p>Date {getLocaleDate()}</p>
 			</div>
-		</div>
+			<div className={styles.wrapper}>
+				<div className={styles.container}>
+					{images.map(([title, src]) => (
+						<Item
+							key={title}
+							title={title}
+							src={src}
+							handleImage={handleModalImage}
+							handleCross={() => {}}
+						/>
+					))}
+				</div>
+			</div>
+			{shouldShow
+				? createPortal(
+						<Modal shouldShow={shouldShow} setShouldShow={setShouldShow}>
+							<img src={image.src} alt={image.title} />
+						</Modal>,
+						document.body
+				  )
+				: null}
+		</>
 	);
 };
 
---
+++ solve/task-2/src/components/modal/modal.tsx
@@ -0,0 +1,26 @@
+import styles from './modal.module.scss';
+import cross from '@assets/svg';
+
+interface IModal {
+	shouldShow: boolean;
+	setShouldShow: (status: boolean) => void;
+	children: React.ReactNode;
+}
+
+const Modal: React.FC<IModal> = ({ shouldShow, setShouldShow, children }) => {
+	return (
+		<div onClick={() => setShouldShow(false)} className={styles.wrapper}>
+			<div onClick={(e) => e.stopPropagation()} className={styles.content}>
+				{children}
+				<img
+					src={cross.cross}
+					alt="cross"
+					className={styles.cross}
+					onClick={() => setShouldShow(false)}
+				/>
+			</div>
+		</div>
+	);
+};
+
+export default Modal;
